555a02a996c4f19642628c2dacd1998438c4852a
1733856225@qq.com <1733856225@qq.com>
2018-08-03 14:06:54 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+  if (window.HTMLElement) {
+    if (Object.getOwnPropertyNames(HTMLElement.prototype).indexOf('dataset') === -1) {
+      Object.defineProperty(HTMLElement.prototype, 'dataset', {
+        get: function () {
+          var attributes = this.attributes
+          var name = []
+          var value = []
+          var obj = {}
+          for (var i = 0; i < attributes.length; i++) {
+            if (attributes[i].nodeName.slice(0, 5) === 'data-') {
+              name.push(attributes[i].nodeName.slice(5))
+              value.push(attributes[i].nodeValue)
+            }
+          }
+          for (var j = 0; j < name.length; j++) {
+            obj[name[j]] = value[j]
+          }
+          return obj
+        }
+      })
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    width: 100%;
+    height: 100%;
+  }
+
+  html, body {
+    width: 100%;
+    height: 100%;
+  }
+</style>
---
+++ src/router/routes.js
@@ -0,0 +1,103 @@
+import Home from '@/components/layout/Home'
+import Index from '@/components/views/Index'
+
+const Chart = () => import('@/components/views/Chart')
+const NotFound = () => import('@/components/pages/NotFound')
+const Register = () => import('@/components/pages/Register')
+const E404 = () => import('@/components/pages/E404')
+const Icon = () => import('@/components/pages/Icon')
+const Login = () => import('@/components/pages/Login')
+const Table = () => import('@/components/views/Table')
+const UserList = () => import('@/components/views/UserList')
+const UserInfo = () => import('@/components/views/UserInfo')
+const List = () => import('@/components/views/List')
+const Filter = () => import('@/components/pages/Filter')
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+    redirect: '/index',
+    children: [  // 所有只有一级的导航放到这里，并指定level=1
+      {
+        path: '/index',
+        name: 'index',
+        component: Index,
+        icon: 'icon-wujiaoxing',
+        level: 1,
+        meta: {
+          requiresAuth: true, // 是否需要登录
+          search: false // 是否可搜索
+        }
+      },
+      {
+        path: '/chart',
+        name: 'chart11',
+        component: Chart
+      },
+      {
+        path: '/list',
+        name: 'list',
+        component: List,
+        meta: {
+          requiresAuth: true, // 是否需要登录
+          search: true // 是否可搜索
+        }
+      },
+      {
+        path: '/userInfo',
+        name: 'userInfo',
+        component: UserInfo,
+        icon: 'icon-addpeople_fill'
+      },
+      {
+        path: '/userList',
+        name: 'userList',
+        component: UserList
+      },
+      {
+        path: '/table',
+        name: 'table',
+        component: Table
+      },
+      {
+        path: '/icon',
+        name: 'icon',
+        component: Icon
+      },
+      {
+        path: '/filter',
+        name: 'filter',
+        component: Filter
+      },
+      {
+        path: '/notFound',
+        name: 'notFound',
+        component: NotFound
+      }
+    ]
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+  {
+    path: '/error404',
+    name: 'error404',
+    component: E404
+  },
+  {
+    path: '*',
+    redirect: '/notFound'
+  }
+
+]
+
+export default routes
